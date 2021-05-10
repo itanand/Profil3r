74ddf4ad3b10a31fc11c24057820390982b2f8e5
Rog3rSm1th <r0g3r5@protonmail.com>
2021-05-10 15:53:33 +0200

    Scrape Pastebin profile

--- profil3r/modules/programming/pastebin.py
+++ profil3r/modules/programming/pastebin.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 import time
 
 class Pastebin:
@@ -36,8 +37,44 @@ class Pastebin:
                 print("failed to connect to pastebin")
             
             # If the account exists
-            if r.status_code == 200:
-                pastebin_usernames["accounts"].append({"value": username})
+            if r.status_code == 200: 
+                # Parse HTML response content with beautiful soup 
+                soup = BeautifulSoup(r.text, 'html.parser')
+                
+                # Scrape the user informations
+                try:
+                    user_profile_views = str(soup.find_all(class_='views')[0].get_text()) or None
+                    user_pastes_views = str(soup.find_all(class_='views')[1].get_text()) or None
+                    user_profile_creation_date = str(soup.find_all(class_='date-text')[0].get_text()) or None
+                except:
+                    pass
+
+                # Scrape the user pastes
+                user_pastes = []
+
+                try:
+                    pastes = soup.find_all(class_='maintable')[0].find_all('tr')
+
+                    for paste in pastes[1:]:
+                        columns = paste.find_all('td')
+
+                        user_pastes.append({
+                            "name": str(columns[0].get_text().strip()),
+                            "added": str(columns[1].get_text().strip()),
+                            "expires": str(columns[2].get_text().strip()),
+                            "hits": str(columns[3].get_text().strip()),
+                            "syntax": str(columns[4].get_text().strip())
+                        })
+                except:
+                    pass
+                
+                # Append all the informations to the account table
+                pastebin_usernames["accounts"].append({"value": username, 
+                                                       "profile_views": user_profile_views,
+                                                       "pastes_views" : user_pastes_views,
+                                                       "profile_creation_date": user_profile_creation_date,
+                                                       "user_pastes": user_pastes
+                                                      })
             time.sleep(self.delay)
         
         return pastebin_usernames
